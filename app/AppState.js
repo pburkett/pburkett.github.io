abc18edaba5786cf60255f169fef6d0bb58c91df
pburkett <pburkett636@gmail.com>
2021-03-05 16:38:34 -0700

    removed extra projects

--- app/AppState.js
+++ app/AppState.js
@@ -5,13 +5,7 @@ import { isValidProp } from "./Utils/isValidProp.js"
 class AppState extends EventEmitter {
   projects = [new Project({
     name: 'Keepr', body: 'Empower users to engage through imagery, and Vaults of their favorites.', id: 0, images: ['keepr.png', 'keepr-modal.png']
-  }), new Project({
-    name: 'Curb-Syde', body: 'Add value by facilitating curbside pickup, online ordering, and customer service.', id: 1, images: ['keepr.png']
-  }), new Project({
-    name: 'Keepr', body: 'Empower users to engage through imagery, and Vaults of their favorites.', id: 2, images: ['keepr.png', 'keepr.png']
-  }), new Project({
-    name: 'Keepr', body: 'Empower users to engage through imagery, and Vaults of their favorites.', id: 3, images: ['keepr.png', 'cs.png']
-  }),]
+  })]
 }
 
 export const ProxyState = new Proxy(new AppState(), {
