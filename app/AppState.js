99f5a6f29f9d697a1585ef7fec5930ac4cfb24cb
pburkett <pburkett636@gmail.com>
2021-03-13 15:43:56 -0700

    added formulator data

--- app/AppState.js
+++ app/AppState.js
@@ -5,12 +5,12 @@ import { isValidProp } from "./Utils/isValidProp.js"
 class AppState extends EventEmitter {
   projects = [new Project({
     name: 'Keepr'
-    , body: 'Empower users to engage through Keeps and Vaults of their favorite keeps ',
+    , body: 'Empower users to engage through Keeps and Vaults of their favorite keeps.',
     id: 0,
     siteLink: 'https://curbsyde.herokuapp.com/',
     gitHubLink: 'https://github.com/pburkett/Keepr',
     images: ['./assets/img/keepr.png', './assets/img/keepr-modal.png'],
-    techs: ['Vue', 'C#', 'DotNet', 'AuthO']
+    techs: ['Vue', 'C#', 'DotNet', 'AuthO', 'Figma']
   }), new Project({
     name: 'Curbsyde'
     , body: 'Facilitate curbside pickup, online ordering, and customer service through a web application. ',
@@ -19,6 +19,14 @@ class AppState extends EventEmitter {
     gitHubLink: 'https://github.com/pburkett/curb-side-fork',
     images: ['./assets/img/activeorders.png', './assets/img/checkout.png'],
     techs: ['Vue', 'Express.js', 'Socket.io', 'AuthO']
+  }), new Project({
+    name: 'Formulator'
+    , body: 'Expedite daily recipe calculations for commercial bakeries.',
+    id: 2,
+    siteLink: 'https://pburkett.github.io/bread-calculator-web-app/',
+    gitHubLink: 'https://github.com/pburkett/bread-calculator-web-app',
+    images: ['./assets/img/formulator.png', './assets/img/formulator-results.png'],
+    techs: ['JavaScript', 'HTML', 'CSS']
   })]
 }
 
